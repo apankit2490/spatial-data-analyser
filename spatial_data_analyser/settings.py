56b6abd3f99fbbb55ff399d9f6f9d9a0f1706dd9
Ankit Kumr Patnaik <apankit2490@gmail.com>
2022-05-24 20:06:53 +0530

    Feat: Configure Django filters and search globally on settings.py
    - Introduce Django filters to enable filtering on viewsets.
    - Include django-filters on requirements.txt.
    - Configure filter_backends, search_fields, filter_fields on WorldCountryViewSet.

--- spatial_data_analyser/settings.py
+++ spatial_data_analyser/settings.py
@@ -233,5 +233,8 @@ CELERY_RESULT_EXPIRES = config(
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 2,
-    
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter'
+    ),
 }
